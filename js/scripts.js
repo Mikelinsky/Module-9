d5c78f2959d8ba4e52bf06d9a475c8726a3563a8
Michał Kupiecki <michal481216@gmail.com>
2017-12-13 23:18:01 +0100

    Zadanie-9.1

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function getTriangleArea(a, h) {
+    if ((a <= 0) && (h <= 0)) {
+        console.log('Incorrect data');
+    }
+    return (a * h / 2);
+}
+console.log(getTriangleArea(10, 6));
+
+var triangleArea = getTriangleArea(10,15);
+console.log(triangleArea);
+
+var triangleArea = getTriangleArea(4,2);
+console.log(triangleArea);
+
+var triangleArea = getTriangleArea(12,36);
+console.log(triangleArea);
+
+var triangleArea = getTriangleArea(0, 4);
+console.log(triangleArea);
