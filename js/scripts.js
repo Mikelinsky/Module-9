c48acd05a500196b2b598f44604342cf7a076d2d
Michał Kupiecki <michal481216@gmail.com>
2017-12-18 13:44:49 +0100

    Zadanie-9.4

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var heightOfTree = prompt('Enter the height');
+function drawTree(heightOfTree) {
+    for (var i = heightOfTree; heightOfTree === heightOfTree; i) {
+            var star = '';
+            for (var j = 0; i <= heightOfTree;) {
+
+            }
+    }
+};
+
+
